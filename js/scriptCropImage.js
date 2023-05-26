b207980481900367b9d587a64057c91a16672bc6
William Richard <richardwilliam02@gmail.com>
2023-05-26 19:21:32 +0700

    add more code for upload image

--- js/scriptCropImage.js
+++ js/scriptCropImage.js
@@ -1,16 +1,12 @@
 const choosenPhoto = document.querySelector("#filePhoto");
 
 const init = function(e){
-    // const reader = new FileReader();
-    // reader.onload() = ()=> {
-        
-    // }
     for(let x=0;x<localStorage.length;x++){
         if(localStorage.key(x)=="image-camera"){
-            choosenPhoto.setAttribute(localStorage.getItem("image-camera"));
+            choosenPhoto.setAttribute('src',localStorage.getItem("image-camera"));
         }
         else if(localStorage.key(x)=="image-gallery"){
-            choosenPhoto.setAttribute(localStorage.getItem("image-gallery"));
+            choosenPhoto.setAttribute('src',localStorage.getItem("image-gallery"));
         }
     }
 }
--- js/scriptUpload.js
+++ js/scriptUpload.js
@@ -3,15 +3,17 @@ const galleryImage = document.querySelector('#galleryFileInput');
 
 cameraInput.addEventListener("change", function(){
     // this.document.getElementById("filePhoto").setAttribute("src",window.URL.createObjectURL(this.files[0]));
-    const fileCamera = this.files[0];
+    console.log(this.files[0]);
+    const fileCamera = URL.createObjectURL(this.files[0]);
     localStorage.setItem("image-camera",fileCamera);
-    window.document.location = 'pages/cropImage.html';
+    window.document.location = 'cropImage.html';
 });
 
 galleryImage.addEventListener("change", function(){
     // this.document.getElementById("filePhoto").setAttribute("src",window.URL.createObjectURL(this.files[0]));
-    const fileGallery = this.files[0];
+    const fileGallery = URL.createObjectURL(this.files[0]);
+    console.log(fileGallery);
     localStorage.setItem("image-gallery",fileGallery);
-    window.document.location = 'pages/cropImage.html';
+    window.document.location = 'cropImage.html';
 })
 
