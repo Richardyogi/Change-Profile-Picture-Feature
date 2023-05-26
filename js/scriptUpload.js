836264316a7748d4c55cb34490b04f33a2b51bad
William Richard <richardwilliam02@gmail.com>
2023-05-26 17:03:30 +0700

    test

--- js/scriptUpload.js
+++ js/scriptUpload.js
@@ -5,13 +5,13 @@ cameraInput.addEventListener("change", function(){
     // this.document.getElementById("filePhoto").setAttribute("src",window.URL.createObjectURL(this.files[0]));
     const fileCamera = this.files[0];
     localStorage.setItem("image-camera",fileCamera);
-    windows.document.location = 'cropImage.html';
+    window.document.location = 'pages/cropImage.html';
 });
 
 galleryImage.addEventListener("change", function(){
     // this.document.getElementById("filePhoto").setAttribute("src",window.URL.createObjectURL(this.files[0]));
     const fileGallery = this.files[0];
     localStorage.setItem("image-gallery",fileGallery);
-    windows.document.location = 'cropImage.html';
+    window.document.location = 'pages/cropImage.html';
 })
 
